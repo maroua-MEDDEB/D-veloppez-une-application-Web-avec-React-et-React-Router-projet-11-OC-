c383cfc14823343b33a5ce98ccfae756595ac1b7
maroua-MEDDEB <70540750+maroua-MEDDEB@users.noreply.github.com>
2023-07-27 11:52:19 +0200

    ajouter l'affichage condiftionné pour l'affichage et le désaffichage du text de collapse

--- src/components/Collapse/Collapse.js
+++ src/components/Collapse/Collapse.js
@@ -9,34 +9,33 @@ const Collapse = (props) =>  {
   //définisser notre état par défaut
   const[open, setOpen] = useState(false);
 
-  const displayCollapseText = () => {
-    setOpen(!open);
-    
-  }
+  const displayCollapseText = () => { setOpen(true)}; // fonction qui affiche le textDeCollpase
+  const unDisplayCollapseText = () => { setOpen(false) } ;// fonction qui désaffiche le textDeCollpase
 
   return (
     <> 
     <div className="collapse_default">
+    {
+      //si l'état du bouton n'est pas ouvert (fermé)   alors on affiche le bouton avec icon downSLine
+      (!open) ?
         <button  className="bouttonDown" type="button" onClick={displayCollapseText}>
           <span> {props.title} </span>
           <RiArrowDownSLine style={{'color':'var(--color--light)'}}/>
         </button>
-
-        {open &&(
-          <>
-          <button  className="bouttonUp" type="button">
-            <span> {props.title} </span>
-            <RiArrowDropUpLine style={{'color':'var(--color--light)'}}/>
-          </button>
-          <div className="collapse_text">
-            {props.text}
-          </div> 
-          </>
-        )}
-
-       
-    </div>
-    </>
+      :
+      //si non alors on affiche le bouton avec icon dropUpLine
+      <>
+      <button  className="bouttonUp" type="button" onClick={unDisplayCollapseText}>
+        <span> {props.title} </span>
+        <RiArrowDropUpLine style={{'color':'var(--color--light)'}}/>
+      </button>
+      <div className="collapse_text">
+        {props.text} 
+      </div> 
+      </>
+    } 
+  </div>
+  </>
   )
 }
 
