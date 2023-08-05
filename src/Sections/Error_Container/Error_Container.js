8e7d99ac08815a88993e7bd18c8f950cb1826b29
maroua-MEDDEB <70540750+maroua-MEDDEB@users.noreply.github.com>
2023-08-05 17:52:29 +0200

    finaliser le css et la responsive de la page d'erreur

--- src/Sections/Error_Container/Error_Container.js
+++ src/Sections/Error_Container/Error_Container.js
@@ -6,10 +6,8 @@ const Error_Container = () => {
     <>
     <div className='error_container'>
       <div className="cod_error">404</div>
-      <div className='text'>
         <div className='text_error'>Oups! La page que vous demandez n'existe pas.</div>
-        <div className='text_return_page'><Link to="/">  Retourner sur la page d’accueil</Link> </div>
-      </div>
+        <Link to="/">  Retourner sur la page d’accueil</Link> 
     </div>
     </>
   )
